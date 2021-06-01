b6c62fa5a111e36e9e38d4293ebb5d90cbe8fd0e
Peeradech Yiengprugsawan <binsgx@gmail.com>
2021-06-01 14:39:25 +0700

    rnew static

--- README.md
+++
@@ -1,2 +0,0 @@
-# edl.web.demo
-
---
+++ content/js/script.js
@@ -0,0 +1,30 @@
+﻿function startVideo(qrReader, resultContainer) {
+
+    docReady(function () {
+        var lastResult, countResults = 0;
+        function onScanSuccess(qrCodeMessage) {
+            if (qrCodeMessage !== lastResult) {
+                ++countResults;
+                lastResult = qrCodeMessage;
+                resultContainer.innerHTML
+                    += `<div>[${countResults}] - ${qrCodeMessage}</div>`;
+            }
+        }
+
+        var html5QrcodeScanner = new Html5QrcodeScanner(
+            qrReader, { fps: 10, qrbox: 250 });
+        html5QrcodeScanner.render(onScanSuccess);
+    });
+}
+
+function docReady(fn) {
+    // see if DOM is already available
+    if (document.readyState === "complete"
+        || document.readyState === "interactive") {
+        // call on next available tick
+        setTimeout(fn, 1);
+    } else {
+        document.addEventListener("DOMContentLoaded", fn);
+    }
+}
+
